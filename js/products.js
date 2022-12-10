288cfeb17e6050fb22c7b861f9f06b2d66528c26
A125X <alexander.andr.shat@gmail.com>
2022-12-10 17:42:50 +0300

    made a cart, design is ugly af though

---
+++ js/products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "MokBook Thicc",
+      desc : "Greatest properly off ham exercise all.",
+      img : "apple 1.png",
+      price : 2034
+    },
+    124: {
+      name : "MokBook Rookie",
+      desc : "Unsatiable its possession nor off.",
+      img : "apple 1.png",
+      price : 1247
+    },
+    125: {
+      name : "iPong Max",
+      desc : "All difficulty unreserved the solicitude.",
+      img : "apple 1.png",
+      price : 675
+    },
+    126: {
+      name : "iTab Pok",
+      desc : "Had judgment out property the supplied. ",
+      img : "apple 1.png",
+      price : 842
+    }
+  };
