f119292180131f3d329016290a2cfef0a40df5e2
A125X <alexander.andr.shat@gmail.com>
2022-12-12 19:05:03 +0300

    cart became a lil bit better

--- js/products.js
+++ js/products.js
@@ -1,27 +1,27 @@
 // DUMMY PRODUCTS (PRODUCT ID : DATA)
 var products = {
     123: {
-      name : "MokBook Thicc",
-      desc : "Greatest properly off ham exercise all.",
-      img : "apple 1.png",
-      price : 2034
+      name : "Lisa 1",
+      desc : "A desktop computer developed by Apple.",
+      img : "lisa.png",
+      price : 5990
     },
     124: {
-      name : "MokBook Rookie",
-      desc : "Unsatiable its possession nor off.",
+      name : "Apple Macintosh",
+      desc : "The original Apple Macintosh personal computer.",
       img : "apple 1.png",
-      price : 1247
+      price : 3490
     },
     125: {
-      name : "iPong Max",
-      desc : "All difficulty unreserved the solicitude.",
-      img : "apple 1.png",
-      price : 675
+      name : "Apple ][",
+      desc : " Highly successful mass-produced microcomputer.",
+      img : "apple 2.png",
+      price : 3990
     },
     126: {
-      name : "iTab Pok",
-      desc : "Had judgment out property the supplied. ",
-      img : "apple 1.png",
-      price : 842
+      name : "Apple ///",
+      desc : "Business-oriented personal computer.",
+      img : "apple 3.png",
+      price : 4490
     }
   };
